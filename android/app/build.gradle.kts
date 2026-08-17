c0e9f1b7ea9b8d90867187a7bc4e253cbf816340
nailaahmad128-source <nailaahmad128@gmail.com>
2026-08-17 19:36:39 +0500

    Test release without R8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
